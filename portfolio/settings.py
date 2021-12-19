5fa8d5958018be5e524e49f0c5fa4b41db599291
Parshu416 <64275709+parshuramsail@users.noreply.github.com>
2021-12-19 21:31:49 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'paresh581345@gmail.com'
-EMAIL_HOST_PASSWORD = '************'
+EMAIL_HOST_PASSWORD = '4SU16EE416'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
